99af113e1c39e0e3b999da4e1127dd31391cf386
Andrii <Luckyhidalgo@gmail.com>
2022-02-03 21:21:43 +0200

    Added wow scroll js animation.

--- src/js/macros/scrollViewPortAnimation.js
+++ src/js/macros/scrollViewPortAnimation.js
@@ -5,7 +5,7 @@ const ScrollViewPortAnimation = (function() {
       animateClass: 'animated',
       offset: 50,
       mobile: true,
-      live: false,
+      live: true,
       callback: function (box) {
         // the callback is fired every time an animation is started
         // the argument that is passed in is the DOM node being animated
